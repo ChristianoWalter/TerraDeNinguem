6f0a05193b2582e818fdae4796e0de37267dd490
unknown <chris.w.lemos03@gmail.com>
2023-02-12 18:43:56 -0300

    commit inicial

---
+++ Assets/Scripts/Game Controllers/PortaController.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortaController : MonoBehaviour
+{
+    public Animator anim;
+
+    public float distanciaParaAbrir;
+
+    private PlayerControl thePlayer;
+
+    private bool playerSaindo;
+
+    public Transform pontoDeSaida;
+
+    public float movePlayerSpeed;
+
+    public string cenaACarregar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thePlayer = PlayerHealthController.instance.GetComponent<PlayerControl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, thePlayer.transform.position) < distanciaParaAbrir)
+        {
+            anim.SetBool("portaAberta", true);
+        }
+        else
+        {
+            anim.SetBool("portaAberta", false);
+        }
+
+        if (playerSaindo)
+        {
+            thePlayer.transform.position = Vector3.MoveTowards(thePlayer.transform.position, pontoDeSaida.position, movePlayerSpeed * Time.deltaTime);
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            if (!playerSaindo)
+            {
+                thePlayer.canMove = false;
+
+                StartCoroutine(UsaPortaCo());
+            }
+        }   
+    }
+
+    IEnumerator UsaPortaCo()
+    {
+        playerSaindo = true;
+
+        //thePlayer.anim.enabled = false;
+
+        UIController.instance.ComecaAEscurecer();
+
+        yield return new WaitForSeconds(1.5f);
+
+        RespawnController.instance.SetSpawn(pontoDeSaida.position);
+        thePlayer.canMove = true;
+        //thePlayer.anim.enabled = true;
+
+        UIController.instance.ComecaAClarear();
+
+
+        PlayerPrefs.SetString("ContinueLevel", cenaACarregar);
+        PlayerPrefs.SetFloat("PositionX", pontoDeSaida.position.x);
+        PlayerPrefs.SetFloat("PositionY", pontoDeSaida.position.y);
+        PlayerPrefs.SetFloat("PositionY", pontoDeSaida.position.z);
+
+
+        SceneManager.LoadScene(cenaACarregar);
+    }
+}
---
+++ Assets/Scripts/Game Controllers/UIController.cs
@@ -0,0 +1,144 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour
+{
+    public static UIController instance;
+    // Start is called before the first frame update
+    public List<GameObject> lifes = new List<GameObject>();
+
+    public GameObject lifeSprite;
+
+    public GameObject lifeGroup;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //transição de cena
+    public Image telaPreta;
+
+    public float fadeSpeed = 2f;
+
+    private bool escurecendo, clareando;
+
+    public string mainMenuScene;
+
+    //public GameObject pauseScreen;
+
+    public GameObject mapaTelaCheia;
+
+    private void Start()
+    {
+        for (int i = 0; i < PlayerHealthController.instance.maxHealth; i++)
+        {
+            GameObject _life = Instantiate(lifeSprite, lifeGroup.transform);
+            lifes.Add(_life);
+        }
+        UpdateSkulls(PlayerHealthController.instance.maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (escurecendo)
+        {
+            telaPreta.color = new Color(telaPreta.color.r, telaPreta.color.g, telaPreta.color.b, Mathf.MoveTowards(telaPreta.color.a, 1f, fadeSpeed * Time.deltaTime));
+            
+            if(telaPreta.color.a == 1f)
+            {
+                escurecendo = false;
+            }
+
+        }else if (clareando) 
+        {
+            telaPreta.color = new Color(telaPreta.color.r, telaPreta.color.g, telaPreta.color.b, Mathf.MoveTowards(telaPreta.color.a, 0f, fadeSpeed * Time.deltaTime));
+
+            if (telaPreta.color.a == 0f)
+            {
+                clareando = false;
+            }
+        }
+
+        
+
+
+       /* if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseUnPause();
+        }*/
+    }
+
+    public void UpdateSkulls(int life)
+    {
+        foreach (var l in lifes)
+        {
+            l.transform.GetChild(0).gameObject.SetActive(false);
+        }
+        for (int i = 0; i < life; i++)
+        {
+            lifes[i].transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+
+    public void ComecaAEscurecer()
+    {
+        escurecendo = true;
+        clareando = false;
+    }
+
+    public void ComecaAClarear()
+    {
+        clareando = true;
+        escurecendo = false;
+    }
+
+
+    /*public void PauseUnPause()
+    {
+        if (!pauseScreen.activeSelf)
+        {
+            pauseScreen.SetActive(true);
+        }
+        else
+        {
+            pauseScreen.SetActive(false);
+        }
+    }*/
+
+    public void ExtraLife()
+    {
+        GameObject _life = Instantiate(lifeSprite, lifeGroup.transform);
+        lifes.Add(_life);
+        UpdateSkulls(PlayerHealthController.instance.currentHealth);
+    }
+
+    public void MainMenu()
+    {
+        Destroy(PlayerHealthController.instance.gameObject);
+        PlayerHealthController.instance = null;
+
+        Destroy(RespawnController.instance.gameObject);
+        RespawnController.instance = null;
+
+       /* Destroy(MapaController.instance.gameObject);
+        MapaController.instance = null;*/
+
+        instance = null;
+        Destroy(gameObject);
+        
+        SceneManager.LoadScene(mainMenuScene);
+    }
+}
